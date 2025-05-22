49851b8ef63f038dcc4d02e9c540514c363645e5
Anton-OG <anton.osiievskyi@gmail.com>
2025-05-22 15:20:06 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/AD/', // ← обязательно с косой чертой!
+  base: './',
   plugins: [react()],
 })
